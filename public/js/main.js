8baeb2bd2dc74ba78361c6b7775e90c0f330eeab
Ido Green <greenido@gmail.com>
2016-04-13 13:42:34 +0300

    add/remove startups

--- public/js/main.js
+++ public/js/main.js
@@ -1,205 +1,280 @@
 (function() {
-    $(".save-alert").hide();
-    $("#spin").hide();
-    var unixTime = Math.floor(Date.now() / 1000);
+  $(".save-alert").hide();
+  $("#spin").hide();
+  var unixTime = Math.floor(Date.now() / 1000);
 
-    //
-    // start the connection with firebase DB
-    //
-    var ref = new Firebase("https://lpa-1.firebaseio.com");
-    authUserData = null;
-    ref.onAuth(function(authData) {
-        if (authData) {
-            authUserData = authData;
-            localStorage.setItem("lpa1-authData", JSON.stringify(authData));
-            console.log("User " + authData.uid + " is logged in with " + authData.provider);
-            $("#login-form").hide();
-            $("#logout-div").html("<form class='navbar-form navbar-right' role='form'><button id='logout-but' class='btn btn-success'>Logout</button> </form>");
-            readMentors(authData);
-        } else {
-            console.log("User is logged out");
-            $("#login-form").show();
-            $("#logout-div").html("");
-        }
-    });
+  //
+  // start the connection with firebase DB
+  //
+  var ref = new Firebase("https://lpa-1.firebaseio.com");
+  authUserData = null;
+  ref.onAuth(function(authData) {
+    if (authData) {
+      authUserData = authData;
+      localStorage.setItem("lpa1-authData", JSON.stringify(authData));
+      console.log("User " + authData.uid + " is logged in with " + authData.provider);
+      $("#login-form").hide();
+      $("#logout-div").html("<form class='navbar-form navbar-right' role='form'><button id='logout-but' class='btn btn-success'>Logout</button> </form>");
+      readMentors(authData);
+    } else {
+      console.log("User is logged out");
+      $("#login-form").show();
+      $("#logout-div").html("");
+    }
+  });
 
-    //
-    // Save mentors
-    //
-    $("#form-save-mentor").click(function() {
-        // validation - TODO: take it out to a function
-        var name = $("#form-name-field").val();
-        var emailKey = $("#form-email-field").val();
-        var tel = $("#form-phone-field").val();
-        // name validation
-        if (name.length < 2) {
-            $("#nameError").html("Please give a name - So you could remember it in the future!");
-            $("#nameError").removeClass("sr-only");
-            $("#nameError").addClass("alert");
-            $("#form-name-field").focus();
-            setTimeout(function() {
-                $("#nameError").removeClass("alert");
-                $("#nameError").addClass("sr-only");
-            }, 1500);
-            return;
-        }
-
-        // email validation
-        if ($("#form-email-field").val().length < 2) {
-            $("#emailError").html("Please give an email - Don't worry we will never spam you.");
-            $("#emailError").removeClass("sr-only");
-            $("#emailError").addClass("alert");
-            $("#form-email-field").focus();
-            setTimeout(function() {
-                $("#emailError").removeClass("alert");
-                $("#emailError").addClass("sr-only");
-            }, 1500);
-            return;
-        }
-        var emailRegEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-        if (!emailRegEx.test(emailKey)) {
-            $("#emailError").html("Please give a valid email (e.g. momo@okko.com");
-            $("#emailError").removeClass("sr-only");
-            $("#emailError").addClass("alert");
-            $("#form-email-field").focus();
-            setTimeout(function() {
-                $("#emailError").removeClass("alert");
-                $("#emailError").addClass("sr-only");
-            }, 1500);
-            return;
-        }
-
-
-        console.log("saving to Firebase: " + name + " , " + email);
-        var curUnixTime = new Date().getTime();
-        var disTime = new Date().toJSON().slice(0, 21);
-
-        //var authData = JSON.parse(localStorage.getItem("lpa1-authData") );
-
-        ref.child("mentors").child(tel).set({
-                name: name,
-                email: emailKey,
-                phone: tel,
-                country: $("#form-country-field").val(),
-                city: $("#form-city-field").val(),
-                domain: $("#form-domain-select option:selected").text(),
-                expertise: $("#form-expertise").val(),
-                linkedin: $("#form-linkedin-url").val(),
-                site: $("#form-personal-url").val(),
-                pic: $("#form-pic-url").val(),
-                comments: $("#form-comments").val(),
-                unixTime: curUnixTime,
-                date: disTime
-            }, function(error) {
-                if (error) {
-                    alert("Data could not be saved :( Details: " + error);
-                } else {
-                    console.log(name + " saved!");
-                    $(".save-alert").show();
-                    setTimeout(function() {
-                        $(".save-alert").hide();
-                    }, 1500);
-                }
-            })
-            // TODO - confirmation on the save
-    });
-
-    //
-    function readMentors(authData) {
-        var readRef = new Firebase("https://lpa-1.firebaseio.com/mentors/");
-        readRef.orderByKey().on("value", function(snapshot) {
-            //console.log("The mentors: " + JSON.stringify(snapshot.val()));
-            $("#mentors-list").html("");
-            snapshot.forEach(function(childSnapshot) {
-                var key = childSnapshot.key();
-                var mentorData = childSnapshot.val();
-                console.log("key: " + key + " data: " + mentorData);
-                $("#mentors-list").append(
-                    '<div class="panel panel-primary"> <div class="panel-heading"> <h3 class="panel-title">' +
-                    mentorData.name + " ( " + mentorData.phone + " )" + '<button type="button" class="remove-mentor btn" aria-label="Close" data-key="' + key + '"> <span aria-hidden="true">&times;</span></button>' +
-                    '</h3> </div> <div class="panel-body mentor-edit" data-key="' + key + '"> ' + mentorData.email + '<br>' +
-                    mentorData.domain + '<br>' + mentorData.expertise + ' </div> </div>'
-                );
-            });
-        });
+  //
+  // Save startups
+  //
+  $("#st-save-button").click(function() {
+    // Validation - TODO: take it out to a function
+    var name = $("#st-name-field").val();
+    var desc = $("#st-desc-field").val();
+    var country = $("#st-country-field").val();
+    // name validation
+    if (name.length < 2) {
+      $("#st-nameError").html("Please give a name - So you could remember this startup in the future!");
+      $("#st-nameError").removeClass("sr-only");
+      $("#st-nameError").addClass("alert");
+      $("#st-name-field").focus();
+      setTimeout(function() {
+        $("#st-nameError").removeClass("alert");
+        $("#st-nameError").addClass("sr-only");
+      }, 1500);
+      return;
     }
 
-    //
-    $("#logout-but").click(function() {
-        ref.unauth();
-        return false;
+    console.log("saving startup to Firebase: " + name + " | desc: " + desc);
+    var curUnixTime = new Date().getTime();
+    var disTime = new Date().toJSON().slice(0, 21);
+
+    ref.child("startups").child(name).set({
+      name: name,
+      description: desc,
+      country: $("#st-country-field").val(),
+      city: $("#st-city-field").val(),
+      fund: $("#st-fund-select option:selected").text(),
+      numEmployees: $("#st-num-employees-select option:selected").text(),
+      dateFounded: $("#st-date-field").val(),
+      logo: $("#st-logo-url").val(),
+      team: $("#st-team-url").val(),
+      video: $("#st-video-url").val(),
+      historyUrl: $("#st-history-url").val(),
+      unixTime: curUnixTime,
+      date: disTime
+    }, function(error) {
+      if (error) {
+        alert("Startup data could not be saved :( Details: " + error);
+      } else {
+        console.log(name + " saved!");
+        $(".save-alert").show();
+        setTimeout(function() {
+          $(".save-alert").hide();
+        }, 1500);
+      }
+    })
+  });
+
+  //
+  // read the list of mentors and display it
+  //
+  function readStartups(authData) {
+    var readRef = new Firebase("https://lpa-1.firebaseio.com/startups/");
+    readRef.orderByKey().on("value", function(snapshot) {
+      console.log("The mentors: " + JSON.stringify(snapshot.val()));
+      $("#startups-list").html("");
+      snapshot.forEach(function(childSnapshot) {
+        var key = childSnapshot.key();
+        var startupData = childSnapshot.val();
+        console.log("key: " + key + " data: " + startupData);
+        $("#startups-list").append(
+          '<div class="panel panel-primary"> <div class="panel-heading"> <h3 class="panel-title">' +
+          startupData.name + " ( " + startupData.logo + " )" + '<button type="button" class="remove-mentor btn" aria-label="Close" data-key="' + key + '"> <span aria-hidden="true">&times;</span></button>' +
+          '</h3> </div> <div class="panel-body mentor-edit" data-key="' + key + '"> ' + startupData.desc + '<br>' +
+          startupData.country + '<br>' + startupData.city + ' </div> </div>'
+        );
+      });
     });
+  }
 
-    // enable to edit mentors from the list
-    $('body').on('click', '.mentor-edit', function(event) {
-        console.log("TODO: edit mentor with key: " + unixTime);
-    });
-
-    // enable removing mentors
-    $('body').on('click', '.remove-mentor', function(event) {
-        var key = this.dataset.key;
-        console.log("going to delete mentor with key: TODO");
-        var fredRef = new Firebase('https://lpa-1.firebaseio.com/mentors/' + key);
-        var onComplete = function(error) {
-            if (error) {
-                console.log('Synchronization failed');
-            } else {
-                console.log('Synchronization succeeded - mentor was removed');
-                $("#mentors-list").html('<div id="loading-mentors"><h2><i class="fa fa-spinner fa-spin"></i> </h2></div>');
-                readMentors(authUserData);
-            }
-        };
-        fredRef.remove(onComplete);
-    });
-
-    //
-    // Sign in
-    //
-    $("#sign-in-but").click(function() {
-        $("#spin").show();
-        var u_email = $("#email").val();
-        var u_passwd = $("#passwd").val();
-        ref.authWithPassword({
-            email: u_email,
-            password: u_passwd
-        }, function(error, authData) {
-            $("#spin").hide();
-            if (error) {
-                console.log("Login Failed!", error);
-                $("#err-modal").modal('show');
-            } else {
-                console.log("Authenticated successfully with payload:", authData);
-            }
-        });
-        return false;
-    });
-
-    //////////////////////////////////////////////////////////////////////////////////
-    // utils
-    //////////////////////////////////////////////////////////////////////////////////
-
-    //
-    function timeConverter(UNIX_timestamp) {
-        var a = new Date(UNIX_timestamp * 1000);
-        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        var year = a.getFullYear();
-        var month = months[a.getMonth()];
-        var date = a.getDate();
-        var hour = a.getHours();
-        var min = a.getMinutes();
-        var sec = a.getSeconds();
-        var time = date + '/' + month + '/' + year + ' - ' + hour + ':' + min + ':' + sec;
-        return time;
+  //
+  // Save mentors
+  //
+  $("#form-save-mentor").click(function() {
+    // validation - TODO: take it out to a function
+    var name = $("#form-name-field").val();
+    var emailKey = $("#form-email-field").val();
+    var tel = $("#form-phone-field").val();
+    // name validation
+    if (name.length < 2) {
+      $("#nameError").html("Please give a name - So you could remember it in the future!");
+      $("#nameError").removeClass("sr-only");
+      $("#nameError").addClass("alert");
+      $("#form-name-field").focus();
+      setTimeout(function() {
+        $("#nameError").removeClass("alert");
+        $("#nameError").addClass("sr-only");
+      }, 1500);
+      return;
     }
 
-    // check for online / lie-fi / offline
-    window.addEventListener("offline", function(e) {
-        console.log('You are OFFLINE');
-        $("#online-status").html(" Offline");
-    }, false);
+    // email validation
+    if ($("#form-email-field").val().length < 2) {
+      $("#emailError").html("Please give an email - Don't worry we will never spam you.");
+      $("#emailError").removeClass("sr-only");
+      $("#emailError").addClass("alert");
+      $("#form-email-field").focus();
+      setTimeout(function() {
+        $("#emailError").removeClass("alert");
+        $("#emailError").addClass("sr-only");
+      }, 1500);
+      return;
+    }
+    var emailRegEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    if (!emailRegEx.test(emailKey)) {
+      $("#emailError").html("Please give a valid email (e.g. momo@okko.com");
+      $("#emailError").removeClass("sr-only");
+      $("#emailError").addClass("alert");
+      $("#form-email-field").focus();
+      setTimeout(function() {
+        $("#emailError").removeClass("alert");
+        $("#emailError").addClass("sr-only");
+      }, 1500);
+      return;
+    }
 
-    window.addEventListener("online", function(e) {
-        console.log("we are back online!");
-        $("#online-status").html(" Online");
-    }, false);
+
+    console.log("saving to Firebase: " + name + " , " + email);
+    var curUnixTime = new Date().getTime();
+    var disTime = new Date().toJSON().slice(0, 21);
+
+    //var authData = JSON.parse(localStorage.getItem("lpa1-authData") );
+
+    ref.child("mentors").child(tel).set({
+      name: name,
+      email: emailKey,
+      phone: tel,
+      country: $("#form-country-field").val(),
+      city: $("#form-city-field").val(),
+      domain: $("#form-domain-select option:selected").text(),
+      expertise: $("#form-expertise").val(),
+      linkedin: $("#form-linkedin-url").val(),
+      site: $("#form-personal-url").val(),
+      pic: $("#form-pic-url").val(),
+      comments: $("#form-comments").val(),
+      unixTime: curUnixTime,
+      date: disTime
+    }, function(error) {
+      if (error) {
+        alert("Data could not be saved :( Details: " + error);
+      } else {
+        console.log(name + " saved!");
+        $(".save-alert").show();
+        setTimeout(function() {
+          $(".save-alert").hide();
+        }, 1500);
+      }
+    })
+  });
+
+  //
+  // read the list of mentors and display it
+  //
+  function readMentors(authData) {
+    var readRef = new Firebase("https://lpa-1.firebaseio.com/mentors/");
+    readRef.orderByKey().on("value", function(snapshot) {
+      //console.log("The mentors: " + JSON.stringify(snapshot.val()));
+      $("#mentors-list").html("");
+      snapshot.forEach(function(childSnapshot) {
+        var key = childSnapshot.key();
+        var mentorData = childSnapshot.val();
+        console.log("key: " + key + " data: " + mentorData);
+        $("#mentors-list").append(
+          '<div class="panel panel-primary"> <div class="panel-heading"> <h3 class="panel-title">' +
+          mentorData.name + " ( " + mentorData.phone + " )" + '<button type="button" class="remove-mentor btn" aria-label="Close" data-key="' + key + '"> <span aria-hidden="true">&times;</span></button>' +
+          '</h3> </div> <div class="panel-body mentor-edit" data-key="' + key + '"> ' + mentorData.email + '<br>' +
+          mentorData.domain + '<br>' + mentorData.expertise + ' </div> </div>'
+        );
+      });
+    });
+  }
+
+  //
+  $("#logout-but").click(function() {
+    ref.unauth();
+    return false;
+  });
+
+  // enable to edit mentors from the list
+  $('body').on('click', '.mentor-edit', function(event) {
+    console.log("TODO: edit mentor with key: " + unixTime);
+  });
+
+  // enable removing mentors
+  $('body').on('click', '.remove-mentor', function(event) {
+    var key = this.dataset.key;
+    console.log("going to delete mentor with key: TODO");
+    var fredRef = new Firebase('https://lpa-1.firebaseio.com/mentors/' + key);
+    var onComplete = function(error) {
+      if (error) {
+        console.log('Synchronization failed');
+      } else {
+        console.log('Synchronization succeeded - mentor was removed');
+        $("#mentors-list").html('<div id="loading-mentors"><h2><i class="fa fa-spinner fa-spin"></i> </h2></div>');
+        readMentors(authUserData);
+      }
+    };
+    fredRef.remove(onComplete);
+  });
+
+  //
+  // Sign in
+  //
+  $("#sign-in-but").click(function() {
+    $("#spin").show();
+    var u_email = $("#email").val();
+    var u_passwd = $("#passwd").val();
+    ref.authWithPassword({
+      email: u_email,
+      password: u_passwd
+    }, function(error, authData) {
+      $("#spin").hide();
+      if (error) {
+        console.log("Login Failed!", error);
+        $("#err-modal").modal('show');
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+    return false;
+  });
+
+  //////////////////////////////////////////////////////////////////////////////////
+  // utils
+  //////////////////////////////////////////////////////////////////////////////////
+
+  //
+  function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + '/' + month + '/' + year + ' - ' + hour + ':' + min + ':' + sec;
+    return time;
+  }
+
+  // check for online / lie-fi / offline
+  window.addEventListener("offline", function(e) {
+    console.log('You are OFFLINE');
+    $("#online-status").html(" Offline");
+  }, false);
+
+  window.addEventListener("online", function(e) {
+    console.log("we are back online!");
+    $("#online-status").html(" Online");
+  }, false);
 })();
